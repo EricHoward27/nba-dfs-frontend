ce81fff828522835e2cdc0d9f99033a11f9c074e
Eric Howard <40253104+EricHoward27@users.noreply.github.com>
2023-10-19 21:00:16 -0600

    add env to ignore; ssc to get user session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Nav from '@/components/Nav'
 import { Button } from '@/components/ui/button'
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from './(auth)/api/auth/[...nextauth]/route';
 
-
-export default function Home() {
+export default async function Home() {
+  // Here we use getServerSession to get the session data that check the user is sign in 
+  const session = await getServerSession(authOptions);
   return (
     <main className="p-24">
-       <Nav />
+       <Nav user={session?.user} expires={session?.expires as string }/>
       <section className='py-12 flex flex-col items-center text-center'>
         <h1 className='text-4xl font-bold'>NBA DFS Lineup Generator</h1>
         <p className='text-2xl text-muted-foreground'>Generate the best possible NBA DFS lineup for the day</p>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { Session } from 'next-auth';
+import { signIn } from 'next-auth/react';
 import { ModeToggle } from "./ui/toggle-mode"
 
 // Nav component for the main navigation bar at the top of the page
 // This component will be reusable across all pages as it can be placed in the layout component and will be rendered on every page
 // Also this component could be used in the main page.tsx file to render the navigation bar on the main page as well
-export default function Nav() {
+export default function Nav( { user }: Session) {
     return (
         <header>
             <nav>
