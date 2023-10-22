785517cd91f2319fcddcc5ac71a740f7fd5be267
Eric Howard <40253104+EricHoward27@users.noreply.github.com>
2023-10-21 18:00:22 -0600

    setup new feat route and component

---
+++ src/app/player-table/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IPlayerTableProps {
+}
+
+export default function PlayerTable (props: IPlayerTableProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
