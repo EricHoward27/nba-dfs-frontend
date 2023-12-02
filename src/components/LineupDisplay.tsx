371e2c258a4f379c6bd622115d07b3b1f0c25c35
Eric Howard <40253104+EricHoward27@users.noreply.github.com>
2023-12-01 19:21:39 -0600

    LineupDisplay component that displays the player card component. Switch component to toggle between draftking and fanduel. Test output and works.

--- src/components/LineupDisplay.tsx
+++ src/components/LineupDisplay.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { Player } from "@/app/types/Player";
-
+import PlayerCard from "./PlayerCard";
 type LineupDisplayProps = {
     lineup: Player[];
 }
 
 const LineupDisplay: React.FC<LineupDisplayProps> = ({ lineup }) => {
+
     return (
         <div>
             {lineup.map((player, index) => (
                 <div key={index}>
-                    <div>{player.Name}</div>
-                    <div>{player.Position}</div>
-                    <div>{player.Points}</div>
-                    <div>{player.FantasyPointsDraftKings}</div>
-                    <div>{player.FantasyPointsFanDuel}</div>
+                    <PlayerCard key={index} player={player} />
                 </div>
             ))}
         </div>
+        
     );
 }
 
---
+++ src/components/PlayerCard.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { Player } from '@/app/types/Player';
+import React, { useState } from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Switch } from "@/components/ui/switch"
+
+
+type PlayerCardProps = {
+    player: Player;
+};
+
+const PlayerCard: React.FC<PlayerCardProps> = ({ player }) => {
+    const [isDraftKings, setIsDraftKings] = useState(true);
+    const handleToggle = () => {
+        setIsDraftKings(!isDraftKings);
+    }
+    return (
+        <Card className='m-4 p-4'>
+            <div className='flex justify-between items-center'>
+                <CardTitle>
+                    {isDraftKings ? 'DraftKings' : 'FanDuel'}
+                    <Switch checked={isDraftKings} onCheckedChange={handleToggle}/>
+                </CardTitle>
+                <CardContent>
+                    <CardHeader>My DFS Lineup</CardHeader>
+                    <CardDescription className='text-lg font-bold'>Name: {player.Name}</CardDescription>
+                    <CardDescription>Position: {player.Position}</CardDescription>
+                    <CardDescription>Points: {player.Points}</CardDescription>
+                    <CardDescription>
+                        Fantasy Points: {isDraftKings ? player.FantasyPointsDraftKings : player.FantasyPointsFanDuel }
+                    </CardDescription>  
+                </CardContent>
+                <CardFooter>
+                    <CardDescription>Projected Points:</CardDescription>
+                </CardFooter>
+            </div>
+        </Card>
+    );
+};
+export default PlayerCard;
+
